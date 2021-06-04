efa44cfdcc1c6864c014d78eb2fa53e2f1114a10
seo1221 <yoonsh552462@naver.com>
2021-06-04 21:35:02 +0900

    게임 오버창

---
+++ gameover.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void gameover()
+{
+	printf("         *****      *     *       * ******   \n");
+	printf("        *          * *    * *   * * *        \n");
+	printf("        *  ****   *****   *  * *  * *****    \n");
+	printf("        *  *  *  *     *  *   *   * *        \n");
+	printf("         *****  *       * *       * ******   \n\n");
+	printf("            ****  *       ****** ****    \n");
+	printf("           *    *  *     * *     *   *   \n");
+	printf("           *    *   *   *  ****  ****    \n");
+	printf("           *    *    * *   *     * *     \n");
+	printf("            ****      *    ***** *   *   \n");
+}
+int main(void)
+{
+	gameover();
+}
